36ba7b582ca14c788b962e556a42f10a001c9c11
Kris Airdancer <chris.scott.marston@gmail.com>
2021-07-12 15:09:14 -0600

    Started work on calculator logic

--- app/src/main/java/com/example/basiccalculator_androidapp/MainActivity.java
+++ app/src/main/java/com/example/basiccalculator_androidapp/MainActivity.java
@@ -3,12 +3,197 @@ package com.example.basiccalculator_androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText calculation, result;
+    private String curr, res; // calculation gets set equal to curr and result gets set equal to res during calculation operations
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDel, btnAC, btnDivision, btnMultiply, btnSubtract, btnAdd, btnDecimal, btnEquals;
+    private boolean dot_inserted, operator_inserted;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculation = findViewById(R.id.calculation);
+        result = (EditText) findViewById(R.id.result);
+
+        curr = "";
+        res = "";
+
+        dot_inserted = false;
+        operator_inserted = false;
+
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btnDel = (Button) findViewById(R.id.btnDel);
+        btnAC = (Button) findViewById(R.id.btnAC);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnDecimal = (Button) findViewById(R.id.btnDecimal);
+        btnEquals = (Button) findViewById(R.id.btnEquals);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "0";
+                displayOne();
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "1";
+                displayOne();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "2";
+                displayOne();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "3";
+                displayOne();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "4";
+                displayOne();
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "5";
+                displayOne();
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "6";
+                displayOne();
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "7";
+                displayOne();
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "8";
+                displayOne();
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "9";
+                displayOne();
+            }
+        });
+
+        // TODO Figure out what logic to give this button
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "";
+                displayOne();
+            }
+        });
+
+        btnAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "";
+                displayOne();
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "÷";
+                displayOne();
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "x";
+                displayOne();
+            }
+        });
+
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "-";
+                displayOne();
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "+";
+                displayOne();
+            }
+        });
+
+
+
+
+
+
     }
+
+    public void displayOne() {
+        calculation.setText(curr);
+    }
+
+    public void displayTwo() {
+        result.setText(res);
+    }
+
+
 }
